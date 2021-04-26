deb337523f80a89300902eef4e2d674c278be80a
slamur <MuravjevSlava@gmail.com>
2021-04-26 18:57:48 +0300

    Added DatabaseData with 'id' column

---
+++ src/main/java/egl/core/model/DatabaseData.java
@@ -0,0 +1,19 @@
+package egl.core.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+import lombok.Data;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@Data
+public class DatabaseData {
+
+    @Id
+    @GeneratedValue
+    private long id;
+}
