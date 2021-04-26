20972742136d956b356e780456d7bbd53cc9b3e6
slamur <MuravjevSlava@gmail.com>
2021-04-27 00:00:17 +0300

    Task: added constructor with parameter

--- src/main/java/egl/core/model/task/Task.java
+++ src/main/java/egl/core/model/task/Task.java
@@ -6,12 +6,19 @@ import javax.persistence.Entity;
 import egl.core.model.DescribedData;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @EqualsAndHashCode(callSuper = true)
 @Entity
 @Data
+@NoArgsConstructor
 public class Task extends DescribedData {
 
     @Column
     private String sceneName;
+
+    public Task(String name, String description, String sceneName) {
+        super(name, description);
+        this.sceneName = sceneName;
+    }
 }
