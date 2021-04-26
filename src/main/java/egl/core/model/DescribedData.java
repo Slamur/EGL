5c66bed095b8ed7687aa6684f44104c6f1fadb59
slamur <MuravjevSlava@gmail.com>
2021-04-27 00:00:03 +0300

    DescribedData: added constructor with parameters

--- src/main/java/egl/core/model/DescribedData.java
+++ src/main/java/egl/core/model/DescribedData.java
@@ -5,15 +5,22 @@ import javax.persistence.Entity;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @EqualsAndHashCode(callSuper = true)
 @Entity
 @Data
-public class DescribedData extends DatabaseData {
+@NoArgsConstructor
+public abstract class DescribedData extends DatabaseData {
 
     @Column
     private String name;
 
     @Column
     private String description;
+
+    protected DescribedData(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
 }
