492b93762bcb0e3ecba5195c1e70d0c23453de26
slamur <MuravjevSlava@gmail.com>
2021-04-26 22:25:28 +0300

    Added (filled) LocalTopicsListController

--- src/main/java/egl/client/controller/topic/LocalTopicsListController.java
+++ src/main/java/egl/client/controller/topic/LocalTopicsListController.java
@@ -4,15 +4,58 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import egl.client.controller.Controller;
+import egl.client.controller.ControllerUtils;
+import egl.client.model.topic.LocalTopic;
+import egl.client.service.FxmlService;
+import egl.client.service.LocalTopicRepository;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import lombok.RequiredArgsConstructor;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.stereotype.Component;
 
 @Component
 @FxmlView
+@RequiredArgsConstructor
 public class LocalTopicsListController extends Controller {
+
+    private final FxmlService fxmlService;
+    private final LocalTopicRepository localTopicRepository;
+
+    @FXML private TableView<LocalTopic> topicsListTableView;
+    @FXML private TableColumn<LocalTopic, String> topicNameColumn;
+    @FXML private TableColumn<LocalTopic, Void> topicLearnColumn;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        initializeColumns();
+    }
 
+    private void initializeColumns() {
+        ControllerUtils.initializePropertyColumn(topicNameColumn, "name");
+
+        ControllerUtils.initializeButtonColumn(
+                topicLearnColumn,
+                "Изучить",
+                (event, topic) -> {
+                    var controllerAndView = fxmlService.showStage(LocalTopicController.class, topic.getName());
+
+                    var topicController = (LocalTopicController) controllerAndView.getController();
+                    topicController.show(topic);
+                }
+        );
+    }
+
+    public void show() {
+        showTopics();
+    }
+
+    private void showTopics() {
+        var tableTopics = topicsListTableView.getItems();
+        tableTopics.clear();
+
+        var databaseTopics = localTopicRepository.findAll();
+        databaseTopics.forEach(tableTopics::add);
     }
 }
