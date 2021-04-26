00db5655cd60180c8d8de2f9a2661a260099a11e
slamur <MuravjevSlava@gmail.com>
2021-04-26 22:01:12 +0300

    TheoryController is abstract now Added CategoryTheoryController

---
+++ src/main/java/egl/client/controller/task/CategoryTheoryController.java
@@ -0,0 +1,51 @@
+package egl.client.controller.task;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import egl.client.model.topic.category.Category;
+import egl.client.model.topic.category.Translation;
+import egl.client.model.topic.category.Word;
+import egl.core.model.task.Task;
+import egl.core.model.topic.Topic;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import net.rgielen.fxweaver.core.FxmlView;
+import org.springframework.stereotype.Component;
+
+@Component
+@FxmlView
+public class CategoryTheoryController extends TheoryController {
+
+    @FXML private TableView<Translation> translationsTableView;
+    @FXML private TableColumn<Translation, Word> sourceTableColumn;
+    @FXML private TableColumn<Translation, Word> targetTableColumn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        super.initialize(url, resourceBundle);
+
+        sourceTableColumn.setCellValueFactory(new PropertyValueFactory<>("source"));
+        targetTableColumn.setCellValueFactory(new PropertyValueFactory<>("target"));
+    }
+
+    @Override
+    protected void prepareToStart(Task controllerTask, Topic controllerTopic) {
+        super.prepareToStart(controllerTask, controllerTopic);
+
+        Category category = (Category) controllerTopic;
+        var categoryTranslations = category.getTranslations();
+
+        if (categoryTranslations.isEmpty()) {
+            translationsTableView.setVisible(false);
+        } else {
+            translationsTableView.setVisible(true);
+
+            var tableTranslations = translationsTableView.getItems();
+            tableTranslations.clear();
+            tableTranslations.addAll(categoryTranslations);
+        }
+    }
+}
--- src/main/java/egl/client/controller/task/TheoryController.java
+++ src/main/java/egl/client/controller/task/TheoryController.java
@@ -3,12 +3,8 @@ package egl.client.controller.task;
 import egl.client.model.topic.LocalTopic;
 import egl.core.model.task.Task;
 import egl.core.model.topic.Topic;
-import net.rgielen.fxweaver.core.FxmlView;
-import org.springframework.stereotype.Component;
 
-@Component
-@FxmlView
-public class TheoryController extends TaskController {
+public abstract class TheoryController extends TaskController {
 
     @Override
     protected void prepareToStart(Task controllerTask, Topic controllerTopic) {
